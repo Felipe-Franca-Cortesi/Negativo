b51bf008840afab1041b8ef491dda7a450aa9f24
Felipe França <contatofelipefranca12@gmail.com>
2020-10-09 13:49:02 -0300

    Adiciona a conclusão do exercício Negativo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Número Negativo\n");
+
+            Console.Write("Digite um número: ");
+            double  numeroDigitado = Convert.ToDouble(Console.ReadLine());
+
+            if (numeroDigitado < 0)
+            {
+                Console.WriteLine("Você digitou um número negativo.");
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Negativo
-[console: Negativo] Leia um número inteiro e exiba a mensagem “Você digitou um número negativo.” caso o número seja menor que zero.
+*[console: Negativo] Leia um número inteiro e exiba a mensagem “Você digitou um número negativo.” caso o número seja menor que zero.*
+
+`Exemplo:`
+
+```
+Digite um número: -12
+Você digitou um número negativo.
+```
+
+`ou`
+
+```
+Digite um número: 12
+```
+(nada a exibir)
